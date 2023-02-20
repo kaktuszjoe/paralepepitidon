14ae8140f7fc216cf88156df8bdd108687c8ea17
Zsiga Gergely <zsi-ga>
2023-02-20 13:03:38 +0100

    adatok bekerese

--- lelepi.java
+++ lelepi.java
@@ -1,5 +1,23 @@
+//Jakabos Ábel 2023 02 20 doga
+
+
+import java.util.Scanner;
+
 public class lelepi {
     public static void main(String[] args) {
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("add meg a paralepid a oldalat");
+
+        double a = myObj.nextDouble();
+        System.out.println("add meg a paralepid a oldalat");
+
+        double b = myObj.nextDouble();
+        System.out.println("add meg a paralepid a oldalat");
+
+        double c = myObj.nextDouble();
         
+        
+    
     }
 }
